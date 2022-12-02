b0b74f8819d82b2fc4e1c680bb959414a107be3e
a.gazalov <gazalov@list.ru>
2022-12-02 16:32:37 +0300

    Task 27 - Sum of digits in the number

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int NumberInput(string invitation)
+{
+    Console.WriteLine(invitation);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int num = NumberInput("Введите число для подсчета суммы цифр в нем:");
+int sum = SumOfDigits(num);
+
+Console.WriteLine($"Сумма цифр в числе {num} равна {sum}");
