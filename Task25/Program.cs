7c1328b0c88bf52d8ea83c7e0a4f40a75a0eabd1
a.gazalov <gazalov@list.ru>
2022-12-02 15:38:56 +0300

    Task 25 - Raising number A to the power B

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int NumberInput(string invitation)
+{
+    Console.WriteLine(invitation);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int PowerRaise(int baseOfPower, int indexOfPower)
+{
+    int result = 1;
+    for (int i = 0; i < indexOfPower; i++)
+    {
+        result = result * baseOfPower;
+    }
+    return result;
+}
+
+int numA = NumberInput("Введите первое число: ");
+int numB = NumberInput("Введите второе натуральное число: ");
+
+int result = PowerRaise(numA, numB);
+
+Console.WriteLine($"Число {numA} в степени {numB} равно: {result}.");
