c92e82cb7a37fe628edb99083de573a742d3ff01
a.gazalov <gazalov@list.ru>
2022-12-02 17:30:12 +0300

    Task29 - Declare an array and print it out to console

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int[] FillArray(int[] array)
+{
+    int arrayLength = array.Length;
+    for (int i = 0; i <= arrayLength - 1; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Массив рандомных чисел из 8 элементов:");
+    Console.Write("[ ");
+    int arrayLength = array.Length;
+    for (int i = 0; i <= arrayLength - 1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+int arrayLength = 8;
+int[] array = new int[arrayLength];
+
+array = FillArray(array);
+PrintArray(array);
